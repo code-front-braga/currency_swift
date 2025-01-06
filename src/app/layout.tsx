76c077501abfa54124f839d518a677c81d91ee26
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-05 21:35:21 -0300

    chore: add custom fonts and define RootLayoutProps type in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
-import './globals.css';
+import { Inter, Alumni_Sans_Pinstripe } from 'next/font/google';
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+import './globals.css';
+import clsx from 'clsx';
+
+const inter = Inter({ subsets: ['latin'], weight: ['400', '700'] });
+const alumniSansPinstripe = Alumni_Sans_Pinstripe({
+	subsets: ['latin'],
+	weight: ['400'],
+});
+
+type RootLayoutProps = {
+	children: Readonly<React.ReactNode>;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en">
-			<body className={`antialiased`}>{children}</body>
+			<body className={clsx('antialiased', inter.className, alumniSansPinstripe.className)}>{children}</body>
 		</html>
 	);
 }
