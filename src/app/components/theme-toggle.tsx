691e3fe66e4d4698aeb6623af75f346022e8b76c
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-05 22:55:15 -0300

    fix: adjust padding from 5rem to 0.5rem

--- src/app/components/theme-toggle.tsx
+++ src/app/components/theme-toggle.tsx
@@ -4,9 +4,9 @@ import { motion } from 'motion/react';
 
 export default function ThemeToggle() {
 	return (
-		<button className="relative flex h-[3rem] w-[5rem] cursor-pointer items-center self-start rounded-full p-[5rem]">
+		<button className="relative flex h-[3rem] w-[5rem] cursor-pointer items-center self-start rounded-full bg-red-500 p-[.5rem]">
 			<motion.div
-				className="h-[2.2rem] w-[2.2rem] rounded-full"
+				className="h-[2.2rem] w-[2.2rem] rounded-full bg-blue-600"
 				layout
 				transition={{ type: 'spring', visualDuration: 0.6, bounce: 0.5 }}
 			/>
