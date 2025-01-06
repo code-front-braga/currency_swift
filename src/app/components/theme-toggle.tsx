e6b34ee6ab9f1fa9a6fa84e42c8c7d08b3c1ba7a
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-05 22:30:22 -0300

    feat: style theme-toggle component with layout (no colors yet)

---
+++ src/app/components/theme-toggle.tsx
@@ -0,0 +1,13 @@
+import { motion } from 'motion/react';
+
+export default function ThemeToggle() {
+	return (
+		<button className="relative flex h-[3rem] w-[5rem] cursor-pointer items-center self-start rounded-full p-[5rem]">
+			<motion.div
+				className="h-[2.2rem] w-[2.2rem] rounded-full"
+				layout
+				transition={{ type: 'spring', visualDuration: 0.6, bounce: 0.5 }}
+			/>
+		</button>
+	);
+}
