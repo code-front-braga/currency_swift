f0e34e9a0e56dd79980d82a955b6123921600ac1
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-06 23:37:06 -0300

    chore: apply colors and darkMode in theme-toggle component

--- src/app/components/theme-toggle.tsx
+++ src/app/components/theme-toggle.tsx
@@ -1,12 +1,29 @@
 'use client';
 
+import clsx from 'clsx';
 import { motion } from 'motion/react';
+import useTheme from '../hooks/useTheme';
 
-export default function ThemeToggle() {
+interface ThemeToggleProps {
+	handleClick: VoidFunction;
+}
+
+export default function ThemeToggle({ handleClick }: ThemeToggleProps) {
+	const { darkMode } = useTheme();
+
 	return (
-		<button className="relative flex h-[3rem] w-[5rem] cursor-pointer items-center self-start rounded-full bg-red-500 p-[.5rem]">
+		<button
+			className={clsx('relative flex h-[3rem] w-[5rem] cursor-pointer items-center self-start rounded-full p-[.5rem]', {
+				'justify-start bg-rebeccaPurple bg-opacity-60': !darkMode,
+				'justify-end bg-orangedRed bg-opacity-50': darkMode,
+			})}
+			onClick={handleClick}
+		>
 			<motion.div
-				className="h-[2.2rem] w-[2.2rem] rounded-full bg-blue-600"
+				className={clsx('h-[2.2rem] w-[2.2rem] rounded-full transition-colors duration-700', {
+					'bg-ghostWhite': !darkMode,
+					'bg-matteBlack': darkMode,
+				})}
 				layout
 				transition={{ type: 'spring', visualDuration: 0.6, bounce: 0.5 }}
 			/>
