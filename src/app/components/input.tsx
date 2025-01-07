01055d553e3a1c4c64b7cc42947339f57f2750c8
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-06 22:27:36 -0300

    chore: add label tag in input component and change type to interface

--- src/app/components/input.tsx
+++ src/app/components/input.tsx
@@ -1,9 +1,25 @@
-type InputProps = {
+interface InputProps {
 	type: string;
 	amount: string;
 	onAmountChange: VoidFunction;
-};
+	labelClassName: string;
+	label: string;
+	inputClassName: string;
+}
 
-export default function Input({ type, amount, onAmountChange, ...props }: InputProps) {
-	return <input type={type} value={amount} onChange={onAmountChange} {...props} />;
+export default function Input({
+	type,
+	amount,
+	onAmountChange,
+	labelClassName,
+	label,
+	inputClassName,
+	...props
+}: InputProps) {
+	return (
+		<>
+			<label className={labelClassName}>{label}</label>
+			<input type={type} value={amount} onChange={onAmountChange} {...props} className={inputClassName} />
+		</>
+	);
 }
