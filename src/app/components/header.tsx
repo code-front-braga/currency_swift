c6299a02379612c8daf458f9744aba3bf998812e
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-06 22:13:28 -0300

    chore: apply colors and darkMode to header component

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,12 +1,34 @@
+import clsx from 'clsx';
 import { CgArrowsExchangeAltV } from 'react-icons/cg';
+import useTheme from '../hooks/useTheme';
 
 export default function Header() {
+	const { darkMode } = useTheme();
+
 	return (
 		<header className="fixed top-[8rem] w-full">
 			<div className="flex items-center justify-center">
-				<CgArrowsExchangeAltV size={36} />
-				<h1 className="font-alumniSansPinstripe text-[3rem] font-semibold">
-					Currency <span className="font-inter">$</span>wift
+				<CgArrowsExchangeAltV
+					size={36}
+					className={clsx({ 'text-rebeccaPurple': !darkMode, 'text-orangedRed': darkMode })}
+				/>
+				<h1
+					className={clsx('font-alumniSansPinstripe text-[3rem] font-semibold', {
+						'text-matteBlack': !darkMode,
+						'dark:from-orangeRed dark:to-orangeRed bg-gradient-to-r text-transparent dark:via-gold dark:bg-clip-text':
+							darkMode,
+					})}
+				>
+					Currency{' '}
+					<span
+						className={clsx('font-inter', {
+							'text-rebeccaPurple': !darkMode,
+							'text-orangedRed': darkMode,
+						})}
+					>
+						$
+					</span>
+					wift
 				</h1>
 			</div>
 		</header>
