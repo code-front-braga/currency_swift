4eab2b0503f4c545c3cf27bca8c9c5297eedb889
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-06 22:18:40 -0300

    chore: apply style and darkMode to form component

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -1,11 +1,22 @@
-interface FormProps extends React.FormHTMLAttributes<HTMLFormElement> {
+import clsx from 'clsx';
+import useTheme from '../hooks/useTheme';
+
+interface FormProps {
 	handleSubmit: VoidFunction;
 	children: React.ReactNode;
 }
 
-export default function Form({ handleSubmit, children, ...props }: FormProps) {
+export default function Form({ handleSubmit, children }: FormProps) {
+	const { darkMode } = useTheme();
+
 	return (
-		<form onSubmit={handleSubmit} {...props}>
+		<form
+			onSubmit={handleSubmit}
+			className={clsx('flex w-full flex-col items-center gap-[3rem] rounded-lg p-4 transition-colors duration-700', {
+				'bg-ghostWhite': !darkMode,
+				'bg-nero': darkMode,
+			})}
+		>
 			{children}
 		</form>
 	);
