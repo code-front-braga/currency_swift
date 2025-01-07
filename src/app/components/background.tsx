a3c02476a8aeb713968ab1ef01011a097388f1f6
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-06 22:05:13 -0300

    chore: apply colors and darkMode to background component

--- src/app/components/background.tsx
+++ src/app/components/background.tsx
@@ -1,11 +1,21 @@
+import clsx from 'clsx';
+import useTheme from '../hooks/useTheme';
+
 export default function Background() {
+	const { darkMode } = useTheme();
+
 	return (
 		<>
 			<div
 				className="absolute inset-0 z-0 bg-repeat"
 				style={{ backgroundImage: 'url(/glitch-effect.png)', backgroundSize: '120px' }}
 			/>
-			<div className="absolute inset-0" />
+			<div
+				className={clsx('absolute inset-0 bg-opacity-80 transition-colors duration-700', {
+					'bg-ghostWhite': !darkMode,
+					'bg-matteBlack': darkMode,
+				})}
+			/>
 		</>
 	);
 }
