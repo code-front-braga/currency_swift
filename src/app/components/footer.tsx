b0b5b3ed5e913fcd04f1b63b5def1a476b4c09e0
Leonardo Braga <programer_frontbraga@outlook.com>
2025-01-07 10:51:21 -0300

    chore: apply colors and darkMode to footer component

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,21 @@
+import clsx from 'clsx';
+import { useTheme } from '../hooks/use-theme';
+
 export default function Footer() {
+	const { darkMode } = useTheme();
 	return (
-		<footer className="fixed bottom-0 w-full rounded-t-[3rem] p-[1.2rem]">
-			<p className="text-center font-inter text-[1.2rem]">
+		<footer
+			className={clsx('fixed bottom-0 w-full rounded-t-[3rem] p-[1.2rem]', {
+				'bg-matteBlack': !darkMode,
+				'bg-ghostWhite': darkMode,
+			})}
+		>
+			<p
+				className={clsx('text-center font-inter text-[1.2rem]', {
+					'text-nero': !darkMode,
+					'text-ghostWhite': darkMode,
+				})}
+			>
 				&copy; {new Date().getFullYear()} Currency Swift. Created by Leonardo Braga.
 			</p>
 		</footer>
